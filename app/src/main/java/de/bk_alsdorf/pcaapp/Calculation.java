f662b3425f4404568260265ab43a02ce946c11b6
Joel Poick <joelpoick@me.com>
2016-11-15 10:30:27 +0100

    Correctly round results to one decimal after comma

--- app/src/main/java/de/bk_alsdorf/pcaapp/Calculation.java
+++ app/src/main/java/de/bk_alsdorf/pcaapp/Calculation.java
@@ -3,35 +3,30 @@ package de.bk_alsdorf.pcaapp;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 
-/**
- * Created by Administrator on 25.10.2016.
- */
-
 public class Calculation {
 
-    // Entered by user
-    private BigDecimal agentPerHour, agentAmount, bolusAmount;
-    private int tankVolume, runtime, bolusPerHour, bolusLock;
-
-    // Calculated
-    private BigDecimal agentAmountPerTank, concentration, basalRate, minimumRuntime = new BigDecimal(0);
-
+    // Wirkstoffmenge = Basalrate in mg/h * 24 * Laufzeit in Tagen
     public static BigDecimal getAgentAmountPerTank(BigDecimal agentPerHour, int runtime) {
-        return agentPerHour.multiply(new BigDecimal(24)).multiply(new BigDecimal(runtime));
+        BigDecimal agentAmountPerTank = agentPerHour.multiply(new BigDecimal(24)).multiply(new BigDecimal(runtime));
+        return agentAmountPerTank.setScale(1, BigDecimal.ROUND_HALF_UP);
     }
 
+    // Basalrate = Basalrate in mg/h * Wirkstoffkonzentration
     public static BigDecimal getBasalRate(BigDecimal agentPerHour, BigDecimal concentration) {
-        return agentPerHour.multiply(concentration);
+        BigDecimal basalRate = agentPerHour.multiply(concentration);
+        return basalRate.setScale(1, BigDecimal.ROUND_HALF_UP);
     }
 
+    // Wirkstoffkonzentration = Wirkstoffmenge / Kassettenvolumen
     public static BigDecimal getConcentration(BigDecimal agentAmount, int tankVolume) {
-        return agentAmount.divide(new BigDecimal(tankVolume));
+        return agentAmount.divide(new BigDecimal(tankVolume), 1, RoundingMode.HALF_UP);
     }
 
+    // Minimale Laufzeit = Kassettenvolumen / ( Basalrate + ( Anzahl Boli pro Stunde * Wirkstoffmenge je Bolus ))
     public static BigDecimal getMinimumRuntime(BigDecimal bolusAmount, int bolusPerHour, BigDecimal basalRate, int tankVolume) {
         BigDecimal maxBoliAgentPerHour = bolusAmount.multiply(new BigDecimal(bolusPerHour));
         BigDecimal maxAgentPerHour = basalRate.add(maxBoliAgentPerHour);
 
-        return new BigDecimal(tankVolume).divide(maxAgentPerHour, 2, RoundingMode.HALF_UP);
+        return new BigDecimal(tankVolume).divide(maxAgentPerHour, 1, RoundingMode.HALF_UP);
     }
 }
