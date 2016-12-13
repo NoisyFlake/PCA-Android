f72998cddecfa29e8914430b6ba12f25727e4cd3
Joel Poick <joelpoick@me.com>
2016-12-13 10:45:30 +0100

    Make Data class more readable

--- app/src/main/java/de/bk_alsdorf/pcaapp/Data.java
+++ app/src/main/java/de/bk_alsdorf/pcaapp/Data.java
@@ -12,33 +12,28 @@ public class Data {
     private static String bolusLock = "0";
     private static String boliPerHour = "0";
 
-    public static String getBasalRate() {
-        return basalRate;
-    }
+    public static String getBasalRate()             { return basalRate; }
+    public static String getCartridge()             { return cartridge; }
+    public static String getDuration()              { return duration;  }
+    public static String getIngredientQuantity()    { return ingredientQuantity; }
+    public static String getDosage()                { return dosage; }
+
+    public static String getBolusAmount()           { return bolusAmount; }
+    public static String getBolusUnit()             { return bolusUnit; }
+    public static String getBolusLock()             { return bolusLock; }
+    public static String getBoliPerHour()           { return boliPerHour; }
 
     public static void setBasalRate(String basalRate) {
         if (basalRate.equals("")) basalRate = "0";
         Data.basalRate = basalRate;
     }
 
-    public static String getCartridge() {
-        return cartridge;
-    }
-
     public static void setCartridge(String cartridge) {
         Data.cartridge = cartridge;
     }
 
-    public static String getDuration() {
-        return duration;
-    }
-
     public static void setDuration(String duration) {
         Data.duration = duration;
-    }
-
-    public static String getIngredientQuantity() {
-        return ingredientQuantity;
     }
 
     public static void setIngredientQuantity(String ingredientQuantity) {
@@ -46,16 +41,8 @@ public class Data {
         Data.ingredientQuantity = ingredientQuantity;
     }
 
-    public static String getDosage() {
-        return dosage;
-    }
-
     public static void setDosage(String dosage) {
         Data.dosage = dosage;
-    }
-
-    public static String getBolusAmount() {
-        return bolusAmount;
     }
 
     public static void setBolusAmount(String bolusAmount) {
@@ -63,25 +50,13 @@ public class Data {
         Data.bolusAmount = bolusAmount;
     }
 
-    public static String getBolusUnit() {
-        return bolusUnit;
-    }
-
     public static void setBolusUnit(String bolusUnit) {
         Data.bolusUnit = bolusUnit;
-    }
-
-    public static String getBolusLock() {
-        return bolusLock;
     }
 
     public static void setBolusLock(String bolusLock) {
         if (bolusLock.equals("")) bolusLock = "0";
         Data.bolusLock = bolusLock;
-    }
-
-    public static String getBoliPerHour() {
-        return boliPerHour;
     }
 
     public static void setBoliPerHour(String boliPerHour) {
